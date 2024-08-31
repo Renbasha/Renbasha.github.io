4e236ed183c977c2920b758ba271725953acfc09
Solène Médina <renbasha.graph@gmail.com>
2024-08-31 14:53:00 +0200

    Delete Caroussel

--- script.js
+++ script.js
@@ -3,33 +3,34 @@
 if (window.top !== window.self) {
     window.top.location = window.location;
   }
-  
-const caroussel = document.querySelector('.caroussel');
-const slides = document.querySelectorAll('.caroussel-images');
-const leftArrow = document.querySelector('.left-arrow');
-const rightArrow = document.querySelector('.right-arrow');
 
-let currentIndex = 0;
+// Caroussel
+// const caroussel = document.querySelector('.caroussel');
+// const slides = document.querySelectorAll('.caroussel-images');
+// const leftArrow = document.querySelector('.left-arrow');
+// const rightArrow = document.querySelector('.right-arrow');
 
-rightArrow.addEventListener('click', () => {
-    if (currentIndex < slides.length - 1) {
-        currentIndex++;
-    } else {
-        currentIndex = 0; // Reviens au début si tu es à la fin
-    }
-    updateCaroussel();
-});
+// let currentIndex = 0;
 
-leftArrow.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-    } else {
-        currentIndex = slides.length - 1; // Va à la fin si tu es au début
-    }
-    updateCaroussel();
-});
+// rightArrow.addEventListener('click', () => {
+//     if (currentIndex < slides.length - 1) {
+//         currentIndex++;
+//     } else {
+//         currentIndex = 0; // Reviens au début si tu es à la fin
+//     }
+//     updateCaroussel();
+// });
 
-function updateCaroussel() {
-    const slideWidth = slides[0].clientWidth;
-    caroussel.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-}
+// leftArrow.addEventListener('click', () => {
+//     if (currentIndex > 0) {
+//         currentIndex--;
+//     } else {
+//         currentIndex = slides.length - 1; // Va à la fin si tu es au début
+//     }
+//     updateCaroussel();
+// });
+
+// function updateCaroussel() {
+//     const slideWidth = slides[0].clientWidth;
+//     caroussel.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+// }
