d37b1f2e6980728babcd831a9567fc4246746b36
Solène Médina <renbasha.graph@gmail.com>
2024-09-02 13:02:09 +0200

    Menu hamb js OMC

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ if (window.top !== window.self) {
     window.top.location = window.location;
   }
 
-  <script>
-document.getElementById('menu-hamb').addEventListener('click', function() {
-    var navLink = document.getElementById('nav-link');
-    navLink.classList.toggle('show');
+// L'icône burger 
+const icone = document.querySelector('.navbar-mobile i');
+console.log(icone);
+// La div modal
+const modal = document.querySelector('.modal');
+console.log(modal);
+
+icone.addEventListener('click', function() {
+  console.log("icone cliquée");
 });
-</script>
+
+icone.addEventListener('click', function(){
+  console.log("icone cliquée");
+  modal.classList.toggle('change-modal');
+});
+
+icone.classList.toggle('fa-times');
+
+// document.getElementById('menu-hamb').addEventListener('click', function() {
+//     var navLink = document.getElementById('nav-link');
+//     navLink.classList.toggle('show');
+// });
+
 
 
 // Caroussel
