f4924d8c52bce7a5fe5bc97c4bce291dbdb3bbfa
Solène Médina <renbasha.graph@gmail.com>
2024-09-04 12:36:42 +0200

    Balise nav manquante

--- script.js
+++ script.js
@@ -4,23 +4,28 @@ if (window.top !== window.self) {
     window.top.location = window.location;
   }
 
+// Trouver le parent direct
+let child = document.querySelector('.child');
+let parent = child.parentElement;
+console.log(parent); // Affiche <div class="parent">...</div>
+
 // L'icône burger 
-const icone = document.querySelector('.navbar-mobile i');
-console.log(icone);
+// const icone = document.querySelector('.navbar-mobile i');
+// console.log(icone);
 // La div modal
-const modal = document.querySelector('.modal');
-console.log(modal);
+// const modal = document.querySelector('.modal');
+// console.log(modal);
 
-icone.addEventListener('click', function() {
-  console.log("icone cliquée");
-});
+// icone.addEventListener('click', function() {
+//   console.log("icone cliquée");
+// });
 
-icone.addEventListener('click', function(){
-  console.log("icone cliquée");
-  modal.classList.toggle('change-modal');
-});
+// icone.addEventListener('click', function(){
+//   console.log("icone cliquée");
+//   modal.classList.toggle('change-modal');
+// });
 
-icone.classList.toggle('fa-times');
+// icone.classList.toggle('fa-times');
 
 // document.getElementById('menu-hamb').addEventListener('click', function() {
 //     var navLink = document.getElementById('nav-link');
